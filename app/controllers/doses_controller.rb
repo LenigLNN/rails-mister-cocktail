1ed06d600307e897876a6e9e3b05ad4136bba011
Lena Nigrell <nigrell@live.se>
2019-06-06 21:41:15 +0200

    adjustments to handle the views better

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,27 +1,33 @@
 class DosesController < ApplicationController
-   def new
+  def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredients = Ingredient.all
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save!
-
-    redirect_to cocktail_path(@dose.cocktail)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    # @dose.ingredient = Ingredient.find(params[:ingredient_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktail/show'
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = @dose.cocktail
     @dose.destroy
 
-    redirect_to cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :doses_id)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
